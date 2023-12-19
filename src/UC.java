ccc17864a51ea24276738e205c8279f0fa961527
AnnaBanana0101 <77004422+AnnaBanana0101@users.noreply.github.com>
2023-12-19 11:01:51 +0530

    Implement UC1

---
+++ src/UC.java
@@ -0,0 +1,18 @@
+
+
+public class UC {
+    public static void main(String[] args)
+    {
+        double value;
+
+        value = Math.random();
+
+        if(value>0.5)
+        {
+            System.out.println("Present");
+        }
+        else{
+            System.out.println("Absent");
+        }
+    }
+}
