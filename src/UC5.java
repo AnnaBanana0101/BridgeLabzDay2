fc1f50c39e14477048e9fdf33054423e6f230cbb
AnnaBanana0101 <77004422+AnnaBanana0101@users.noreply.github.com>
2023-12-19 18:20:03 +0530

    UC5

---
+++ src/UC5.java
@@ -0,0 +1,15 @@
+
+public class UC5 {
+    public static void main(String[] args) {
+        int monthly_wage_value;
+
+        int hours = 20;
+        int wage = 8;
+        int days = 20;
+
+        monthly_wage_value = hours * wage * days;
+
+        System.out.println(monthly_wage_value);
+
+    }
+}
