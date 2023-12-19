a7c0ee8a02e18860e3e2488bbadbcb248e98cc78
AnnaBanana0101 <77004422+AnnaBanana0101@users.noreply.github.com>
2023-12-19 18:17:31 +0530

    UC4

---
+++ src/UC4.java
@@ -0,0 +1,20 @@
+
+public class UC4 {
+    public static void main(String[] args) {
+        int part_time_wage_value;
+
+        int hours = 8;
+        int wage = 8;
+
+        switch (hours) {
+            case 8:
+                System.out.println("Part Time");
+                break;
+
+            case 20:
+                System.out.println("Full Time");
+                break;
+        }
+
+    }
+}
