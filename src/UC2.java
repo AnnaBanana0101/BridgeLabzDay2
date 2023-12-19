631e38b0e9f1bca5fde4e3f91139e1132c4ce450
AnnaBanana0101 <77004422+AnnaBanana0101@users.noreply.github.com>
2023-12-19 18:09:50 +0530

    UC2

---
+++ src/UC2.java
@@ -0,0 +1,14 @@
+
+public class UC2 {
+    public static void main(String[] args) {
+        int wage_value;
+
+        int hours = 8;
+        int wage = 20;
+
+        wage_value = hours * wage;
+
+        System.out.println(wage_value);
+
+    }
+}
