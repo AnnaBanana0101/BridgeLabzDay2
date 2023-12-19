9d1b42212dded7e86ee0d6940ee26c273e6fa684
AnnaBanana0101 <77004422+AnnaBanana0101@users.noreply.github.com>
2023-12-19 18:13:54 +0530

    UC3

---
+++ src/UC3.java
@@ -0,0 +1,14 @@
+
+public class UC3 {
+    public static void main(String[] args) {
+        int part_time_wage_value;
+
+        int hours = 8;
+        int wage = 8;
+
+        part_time_wage_value = hours * wage;
+
+        System.out.println(part_time_wage_value);
+
+    }
+}
